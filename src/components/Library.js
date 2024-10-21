fdb5b51c34b6b0535fafbaf96d280f851a561cd8
Gladstone Rose <gladstonerose@hotmail.com>
2024-10-20 22:15:08 -0500

    delete AddMediaForm.js and modify Library.js

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,18 +1,38 @@
 import React, { useState } from 'react';
 import MediaList from './MediaList';
-import AddMediaForm from './AddMediaForm';
 
 function Library() {
-  const [mediaItems, setMediaItems] = useState([]);
-
-  const addMediaItem = (newItem) => {
-    setMediaItems([...mediaItems, newItem]);
-  };
+  // Hardcoded media items
+  const [mediaItems] = useState([
+    {
+      type: 'Book',
+      title: 'Harry Potter',
+      author: 'J.K. Rowling',
+      pages: 500,
+      isCheckedOut: false,
+      ratings: [5, 4]
+    },
+    {
+      type: 'Movie',
+      title: 'Jurassic Park',
+      director: 'Steven Spielberg',
+      runTime: 127,
+      isCheckedOut: false,
+      ratings: [5]
+    },
+    {
+      type: 'CD',
+      title: 'Abbey Road',
+      artist: 'The Beatles',
+      songs: ['Come Together', 'Something'],
+      isCheckedOut: false,
+      ratings: [5, 4]
+    }
+  ]);
 
   return (
     <div className="library">
       <h2>Library Collection</h2>
-      <AddMediaForm addMediaItem={addMediaItem} />
       <MediaList mediaItems={mediaItems} />
     </div>
   );
