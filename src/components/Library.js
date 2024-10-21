0626db99c3cc120d9ca06ce1fc0024182360233c
Gladstone Rose <gladstonerose@hotmail.com>
2024-10-21 08:09:12 -0500

    modify Library.js to add more media

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,5 +1,3 @@
-// Library.js
-
 import React, { useState } from 'react';
 import MediaList from './MediaList';
 
@@ -15,6 +13,22 @@ function Library() {
       ratings: [5, 4]
     },
     {
+      type: 'Book',
+      title: 'The Hobbit',
+      author: 'J.R.R. Tolkien',
+      pages: 310,
+      isCheckedOut: false,
+      ratings: [5, 5, 4]
+    },
+    {
+      type: 'Book',
+      title: 'To Kill a Mockingbird',
+      author: 'Harper Lee',
+      pages: 324,
+      isCheckedOut: false,
+      ratings: [4, 5, 5]
+    },
+    {
       type: 'Movie',
       title: 'Jurassic Park',
       director: 'Steven Spielberg',
@@ -23,12 +37,44 @@ function Library() {
       ratings: [5]
     },
     {
+      type: 'Movie',
+      title: 'Inception',
+      director: 'Christopher Nolan',
+      runTime: 148,
+      isCheckedOut: false,
+      ratings: [5, 4, 5]
+    },
+    {
+      type: 'Movie',
+      title: 'The Matrix',
+      director: 'Lana Wachowski, Lilly Wachowski',
+      runTime: 136,
+      isCheckedOut: false,
+      ratings: [5, 5, 4]
+    },
+    {
       type: 'CD',
       title: 'Abbey Road',
       artist: 'The Beatles',
       songs: ['Come Together', 'Something'],
       isCheckedOut: false,
       ratings: [5, 4]
+    },
+    {
+      type: 'CD',
+      title: 'Thriller',
+      artist: 'Michael Jackson',
+      songs: ['Thriller', 'Beat It', 'Billie Jean'],
+      isCheckedOut: false,
+      ratings: [5, 5, 5]
+    },
+    {
+      type: 'CD',
+      title: 'Back in Black',
+      artist: 'AC/DC',
+      songs: ['Back in Black', 'You Shook Me All Night Long'],
+      isCheckedOut: false,
+      ratings: [4, 5, 5]
     }
   ]);
 
